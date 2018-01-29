e603798c4f70c784e93bfe6b5100b88d8488fd15
tramplpf <3013470059@qq.com>
2018-01-29 15:24:44 +0800

    commit Hello.java file

--- src/java/com/lpf/demo/Hello.java
+++ src/java/com/lpf/demo/Hello.java
@@ -3,10 +3,11 @@ package com.lpf.demo;
 public class Hello{
 	public static void main(String[] args){
 		System.out.println("HelloWorld!");
-		System.out.println("Hello!1");
+		System.out.println("Hello!0");
 		System.out.println("Hello!2");
 		System.out.println("Hello!3");
 		System.out.println("Hello!4");
+		System.out.println("Hello!5");
 		System.out.println("Hello!5");
 	}
 }
